7f49915d2b0eb374b55b6f29e455c0ccea635d69
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-10-13 14:30:58 -0700

    Tasks Scripts of alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *   main - Prints "Programming is like building a multilingual
+ * main - Prints "Programming is like building a multilingual
  *
  *                   puzzle, followed by a new line.   
  *
