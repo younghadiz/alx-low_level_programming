7d1ba68f8b4be2a6f87a93ec16bc172651f30e61
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-10-21 10:24:30 -0700

    Solution to tasks of 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,38 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers between 0 to 14
- * 10 times.
- * Return: no return.
- **/
+ *
+ *  * more_numbers - Prints the numbers 0-14 ten times.
+ *
+ *   */
 
 void more_numbers(void)
 
 {
-	int i, ch;
-	for (i = 0; i < 10; i++)
-	{
-		for (ch = 0; ch < 15; ch++)
-		{
-			if (ch >= 10)
-																			_putchar((ch / 10) + 48);
-																			_putchar((ch % 10) + 48);
-																		}
-		_putchar('\n');
-	}
+
+		int num, count;
+
+
+
+			for (count = 0; count <= 9; count++)
+
+					{
+
+								for (num = 0; num <= 14; num++)
+
+											{
+
+															if (num > 9)
+
+																				_putchar((num / 10) + '0');
+
+																		_putchar((num % 10) + '0');
+
+																				}
+
+										_putchar('\n');
+
+											}
+
 }
 
