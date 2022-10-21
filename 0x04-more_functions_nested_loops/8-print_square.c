2c366bdb6633df2de78e5e7886ee2bf8d68a5ba6
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-10-21 09:41:01 -0700

    Solution to tasks of 0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ * print_square - prints hashes squares.
+ * @size: size of the square.
+ * Return: no return.
+ **/
+
+void print_square(int size)
+
+{
+	int i, j;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putcha(35);
+		}
+		if (i != size - 1)
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
