7545594a814c3aa20899c9cec7200f06e8e46530
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-10-31 21:41:05 -0700

    Solutions to all tasks of 0x07 C Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,4 @@
-#include "main.h"
-
-
+i#include "main.h"
 
 /**
  *
@@ -8,11 +6,16 @@
  *
  *   *           to by @src into that pointed to by @dest.
  *
- *    *
+ *    * @dest: A pointer to the memory area to copy @src into.
  *
- *     * Return: A pointer to the destination buffer @dest.
+ *     * @src: The source buffer to copy characters from.
  *
- *      */
+ *      * @n: The number of bytes to copy from @src.
+ *
+ *       *
+ *
+ *        * Return: A pointer to the destination buffer @dest.
+ */
 
 void *_memcpy(void *dest, const void *src, size_t n)
 
