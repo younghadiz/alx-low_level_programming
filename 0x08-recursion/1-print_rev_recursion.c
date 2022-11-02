7db573d4d2e9969330557946958d03807a39ec58
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-11-02 14:37:37 -0700

    Solutions to all tasks of 0x08 C Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,32 +1,28 @@
 #include "main.h"
 
+
+
 /**
  *
- *  * _puts_recursion - prints a string, followed by a new line.
+ *  * _print_rev_recursion - Prints a string in reverse.
  *
- *   * @s: string
+ *   * @s: The string to be printed.
  *
- *    * Return: no return.
- *
- *     */
+ *    */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 
 {
 
-		if (*s != '\0')
+		if (*s)
 
 				{
 
-							_putchar(*s);
+							_print_rev_recursion(s + 1);
 
-									_puts_recursion(s + 1);
+									_putchar(*s);
 
 										}
-
-			else
-
-						_putchar('\n');
 
 }
 
