907402d1ad54b3a11009f60c42fbdee6d8f53e50
Salaudeen Gafari Oladele <gafarioladele1@gmail.com>
2022-11-15 14:03:20 -0800

    Solutions to all tasks of 0x0D C Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - prints the name of the file.
+ *main - Prints the name of the file of the program
+ *was compiled from, followed by a new line.
  *Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n", FILE);
+	printf("%s\n", __FILE__);
 	return (0);
 }
+
